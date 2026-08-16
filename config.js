76f0c7c43444bef91199ee37ab4192cca5c39300
jitxpoint-hub <jitxpoint-hub@users.noreply.github.com>
2026-08-16 17:18:40 +0330

    Refresh banner feed every four hours

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ window.XPOINT_CONFIG = {
   // آدرس Web App ساخته‌شده با فایل google-apps-script/BannerFeed.gs
   bannerFeedUrl: "https://script.google.com/macros/s/AKfycbwSDnekEJneELVjWHgNL_R67y2wYbmu8JFiB3FXFqIdbyOY2_dtH6ypGruHbtGD9U4n/exec",
   bannerRotationMs: 5500,
-  bannerRefreshMs: 300000,
+  bannerRefreshMs: 14400000,
   fallbackDataUrl: "products.sample.json",
   currencyLabel: "تومان"
 };
